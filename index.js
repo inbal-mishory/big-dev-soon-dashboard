e6d61f885c7a62583685f6c5d8bbb4991304b0ea
Inbal Mishory <mishory@gmail.com>
2024-09-04 13:18:57 +0300

    Get BE data for cards, display cards when data recieved

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
+  // DOM variables
   const main = document.querySelector("main");
   const sidebar = document.querySelector(".sidebar");
+  const cardsElement = document.querySelector(".cards-container");
+
+  // Get Data from API
+
+  const apiUrl = 'https://dummyjson.com/c/0bbf-1685-443d-b67c';
+  window.fetch(apiUrl).then((response) => {
+    return response.json();
+  }).then((data) => {
+    const cards = data[0]["General data"];
+    const charts = data[1];
+    const integration = data[2];
+
+    cards.forEach(card => {
+      const cardElement = `<div class="card">
+              <h3>${card.title}</h3>
+              <div class="value">${card.cardvalue}</div>
+              <div class="change">
+                <div class="trend ${card.trend}">${card.change}</div>
+                <div>Compared to last month</div>
+              </div>
+            </div>`
+      cardsElement.innerHTML += cardElement;
+    });
+  })
+
+  // Sidebar open/close beahvior
   document.querySelector(".toggle-sidebar").addEventListener("click", (e) => {
     if(main.classList.contains("side-open")) {
       main.classList.remove("side-open");
